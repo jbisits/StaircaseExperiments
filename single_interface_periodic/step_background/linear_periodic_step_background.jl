f632439a28074b7cb245edf8d3d1dcd65c5f6557
jbisits <jbisits@gmail.com>
2024-12-06 10:00:49 +1100

    More noise

--- single_interface_periodic/step_background/linear_periodic_step_background.jl
+++ single_interface_periodic/step_background/linear_periodic_step_background.jl
@@ -14,7 +14,7 @@ salinity = [34.56, 34.70]
 temperature = [-1.5, 0.5]
 interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature,
                                     background_state = BackgroundStep())
-noise = (velocities = VelocityNoise(1e-7), tracers = TracerNoise(1e-6, 1e-6))
+noise = (velocities = VelocityNoise(1e-2), tracers = TracerNoise(1e-6, 1e-6))
 
 ## setup model
 sdns = StaircaseDNS(model_setup, interface_ics, noise)
