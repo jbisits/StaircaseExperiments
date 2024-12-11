f032a71a53eb52e6d15bd65633bcaa81bd5981a5
jbisits <jbisits@gmail.com>
2024-12-11 13:31:04 +1100

    Different ref density for nl + lower R sub rho for linear

--- single_interface_periodic/tanh_background/linear_tanh_background.jl
+++ single_interface_periodic/tanh_background/linear_tanh_background.jl
@@ -12,7 +12,7 @@ model_setup = (;architecture, diffusivities, domain_extent, domain_topology, res
 
 ## Initial conditions
 depth_of_interface = -0.5
-salinity = [34.56, 34.70]
+salinity = [34.54, 34.70]
 temperature = [-1.5, 0.5]
 interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature,
                                     background_state = BackgroundTanh(50))
