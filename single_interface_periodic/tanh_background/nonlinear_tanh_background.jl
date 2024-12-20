f7d0aeaced9d500d01377f05c13445b4d7d42ab2
jbisits <jbisits@gmail.com>
2024-12-20 21:44:46 +1100

    Update nonlinear_tanh_background.jl

--- single_interface_periodic/tanh_background/nonlinear_tanh_background.jl
+++ single_interface_periodic/tanh_background/nonlinear_tanh_background.jl
@@ -17,7 +17,7 @@ depth_of_interface = -0.5
 salinity = [34.58, 34.7]
 temperature = [-1.5, 0.5]
 interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature,
-                                    background_state = BackgroundTanh(5000))
+                                    background_state = BackgroundTanh(100))
 noise = VelocityNoise(1e-2)
 
 ## setup model
