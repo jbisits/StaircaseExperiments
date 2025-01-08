7f9c6469126d9a0c94a9947c7160d7b5e01e4f74
jbisits <jbisits@gmail.com>
2025-01-08 12:17:52 +1100

    Lower R_rho

--- single_interface_jump/linear_jump_periodic.jl
+++ single_interface_jump/linear_jump_periodic.jl
@@ -13,7 +13,7 @@ model_setup = (;architecture, diffusivities, domain_extent, domain_topology, res
 
 ## Initial conditions
 depth_of_interface = -0.5
-salinity = [34.56, 34.7]
+salinity = [34.54, 34.7]
 temperature = [-1.5, 0.5]
 interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature,
                                     interface_smoothing = TanhInterfaceSteepness(100.0))
@@ -23,7 +23,7 @@ noise = VelocityNoise(1e-2)
 sdns = StaircaseDNS(model_setup, interface_ics, noise)
 
 ## Build simulation
-stop_time = 6 * 60 * 60 # seconds
+stop_time = 8 * 60 * 60 # seconds
 output_path = joinpath(@__DIR__, "Velocity_noise_$(round(interface_ics.R_ρ, digits = 2))")
 checkpointer_time_interval = 60 * 60 # seconds
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
@@ -31,7 +31,7 @@ simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                     output_path, checkpointer_time_interval,
                                     overwrite_saved_output = restart)
 ## Run
-simulation.stop_time = 12 * 60 * 60 # update to pickup from a checkpoint
+# simulation.stop_time = 12 * 60 * 60 # update to pickup from a checkpoint
 pickup = restart ? false : readdir(simulation.output_writers[:checkpointer].dir, join = true)[1]
 run!(simulation; pickup)
 
