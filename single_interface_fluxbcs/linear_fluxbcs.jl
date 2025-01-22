765de10010ba6288da3f5d5db21ce86c86a5d74d
jbisits <jbisits@gmail.com>
2025-01-22 13:56:01 +1100

    Stronger fluxes (maybe maintains better?)

--- single_interface_fluxbcs/linear_fluxbcs.jl
+++ single_interface_fluxbcs/linear_fluxbcs.jl
@@ -10,9 +10,9 @@ eos = CustomLinearEquationOfState(-0.5, 34.6, reference_density = ρ₀)
 model_setup = (;architecture, diffusivities, domain_extent, domain_topology, resolution, eos)
 # bcs from a rundown model and are an approximation/test to see if can simulate
 # effect of interfaces either side.
-Jᵀ = 2e-5
+Jᵀ = 3.5e-5
 T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Jᵀ), bottom = FluxBoundaryCondition(Jᵀ))
-Jˢ = 1.6e-7
+Jˢ = 2.5e-7
 S_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Jˢ), bottom = FluxBoundaryCondition(Jˢ))
 boundary_conditions = (T=T_bcs, S=S_bcs)
 dns_model = DNSModel(model_setup...; boundary_conditions)
