20c70cee49f1f6f939d074aa6787219108457c46
jbisits <jbisits@gmail.com>
2025-02-19 16:43:24 +1100

    Lower R_rho

--- notebooks/diffusive_interfaces_2.jl
+++ notebooks/diffusive_interfaces_2.jl
@@ -60,7 +60,8 @@ begin
 	Sₘ, Θₘ = 0.5 * (S✶ + Sᵤ), 0.5 * (Θ✶ + Θᵤ)
 	Lz = -1
 	interface = Lz / 2
-	z = range(Lz, 0, length = 1000)
+	Nz = 1000
+	z = range(Lz, 0, length = Nz)
 	scale = 100
 	initial_S = @. Sᵤ + (ΔS / 2) * (1 + tanh(100 * (z - interface)) / abs(Lz))
 	reverse!(initial_S)
@@ -70,7 +71,7 @@ begin
 	intial_ρ′ = (intial_ρ .- intial_ρ[end]) ./ norm(intial_ρ)
 	max_ρ, max_idx = findmax(intial_ρ)
 
-	S_range = range(Sᵤ, S✶, length = 1000)
+	S_range = range(Sᵤ, S✶, length = Nz)
 	mixing_line = @. Θ✶ + (ΔΘ / ΔS) * (S_range - S✶)
 	ρ_mixing = gsw_sigma0.(S_range, mixing_line)
 	max_ρ_mix, mix_idx = findmax(ρ_mixing)
@@ -181,6 +182,90 @@ let
 	fig
 end
 
+# ╔═╡ c80a9808-e582-438a-b903-3e2cb32d9505
+md"""
+# Getting same background density gradient with linear and non-linear eos
+
+The above figure shows that setting linear ``S`` and ``\Theta`` background states gives *very different background density state.*
+This must be why in the linear case we see staircases form but nothing happens in the non-linear eos.
+
+What I would like to do is to find ``S`` and ``\Theta`` background states that give constant density background
+"""
+
+# ╔═╡ c095e71b-bbb2-4586-aa0b-ce6c0f1ce74b
+begin
+	ρᵤ = gsw_rho(Sᵤ, Θᵤ, 0)
+	ρ✶ = gsw_rho(S✶, Θ✶, 0)
+	ρ_background = range(ρ✶, ρᵤ, length = Nz)
+	Δρ = diff(ρ_background)
+	Θ_range = range(Θᵤ, Θ✶, length = Nz)
+	S_grid = S_range .* ones(Nz)'
+	Θ_grid = ones(Nz) .* Θ_range'
+	ρ_grid = gsw_rho.(S_grid, Θ_grid, 0)
+	
+	f, a, hm = contourf(S_range, Θ_range, ρ_grid, colormap = :dense)
+	contour!(a, S_range, Θ_range, ρ_grid, levels = [ρᵤ, ρ✶], color = :red)
+	a.xlabel = "S (gkg⁻¹)"
+	a.ylabel = "Θ (°C)"
+	Colorbar(f[1, 2], hm)
+
+	# find_ = [findfirst(ρ_b .≈ ρ_grid) for (i, ρ_b) ∈ enumerate(ρ_background)]
+
+	find_Θ = [findfirst(gsw_rho.(S_range[1+i],  Θ_range, 0) .<= ρ_background[end-i]) for i ∈ 1:Nz-2]
+	Θ_ = vcat(Θ_range[1], Θ_range[find_Θ], Θ_range[end])
+	ρ_ = gsw_rho.(S_range, Θ_ , 0)
+	Δρ_ = diff(ρ_)
+	lines!(a, S_range, Θ_, color = :orange)
+	# for i ∈ eachindex(ρ_background)
+	# 	lines!(a, S_grid[find_[i]], Θ_grid[find_[i]], color = :red)
+	# end
+	# for i ∈ eachindex(ρ_background)
+	# 	scatter!(a, S_grid[find_[i]], Θ_grid[find_[i]], color = :red)
+	# end
+	# for i ∈ eachindex(ρ_background)
+	# 	lines!(a, S_range[find_[i][1][1]], Θ_range[find_[i][1][2]], color = :orange)
+	# end
+	f
+
+end
+
+# ╔═╡ 5b0e30d6-9808-45a4-b297-432e752c5181
+let
+	fig = Figure(size = (1000,1000))
+	ax1 = Axis(fig[1, 1],
+			    xticklabelcolor = :blue,
+			    bottomspinecolor = :blue,
+			    xtickcolor = :blue,
+				xlabelcolor = :blue,
+				xlabel = "S (gkg⁻¹)",
+				ylabel = "z (m)")
+	ax2 = Axis(fig[1, 1],
+				xaxisposition = :top,
+			    xticklabelcolor = :red,
+			    topspinecolor = :red,
+			    xtickcolor = :red,
+				xlabelcolor = :red,
+				xlabel = "Θ (°C)")
+	lines!(ax1, reverse(S_range), z, color = :blue)
+	lines!(ax2, reverse(Θ_), z, color = :red, linestyle = :dash)
+
+	ax3 = Axis(fig[1, 2],
+				xlabel = "σ₀′ (nondim)")
+	lines!(ax3, reverse(ρ_), z, label = "Nonlinear eos")
+	# lines!(ax3, ρ_linear_bg_lineareos, z, label = "Linear eos")
+	axislegend(ax3)
+	ax4 = Axis(fig[2, :], xlabel = "S (gkg⁻¹)", ylabel = "Θ (°C)")
+	lines!(ax4,  S_linear_bg, Θ_linear_bg)
+	lines!(ax4, S_range, Θ_, color = :orange)
+	# scatter!(ax4, initial_S[max_idx], initial_Θ[max_idx], color = :orange)
+	# lines!(ax4, S_range, mixing_line, color = :purple, linestyle = :dash)
+	# scatter!(ax4, S_range[mix_idx], mixing_line[mix_idx], color = :red)
+	fig
+end
+
+# ╔═╡ e2dd28a5-fd27-410a-9a87-a5be7299d72b
+TableOfContents()
+
 # ╔═╡ Cell order:
 # ╟─189277c0-ecdf-11ef-0b11-ad7fc2913d2d
 # ╟─b8342884-a419-46ff-a2cf-3c7caac2f902
@@ -191,3 +276,7 @@ end
 # ╟─67e66c9a-309b-43f3-8956-92404d77a3ef
 # ╟─67fc284f-71d1-494a-bdcf-efff768f0683
 # ╟─f126f7a6-5e86-4f4c-a36f-10b9cf5228b4
+# ╟─c80a9808-e582-438a-b903-3e2cb32d9505
+# ╟─c095e71b-bbb2-4586-aa0b-ce6c0f1ce74b
+# ╟─5b0e30d6-9808-45a4-b297-432e752c5181
+# ╟─e2dd28a5-fd27-410a-9a87-a5be7299d72b
