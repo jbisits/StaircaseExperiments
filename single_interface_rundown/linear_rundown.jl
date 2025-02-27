18a99857e45c046cc14b2c132b1c4b60adac8969
jbisits <jbisits@gmail.com>
2025-02-28 09:18:46 +1100

    Path with initial state

--- single_interface_rundown/linear_rundown.jl
+++ single_interface_rundown/linear_rundown.jl
@@ -24,7 +24,8 @@ sdns = StaircaseDNS(dns_model, interface_ics, initial_noise = noise)
 
 ## Build simulation
 stop_time = 8 * 60 * 60 # seconds
-output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))")
+initial_state = interface_ics.interface_smoothing <: NoSmoothing ? "step" : "tanh"
+output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 checkpointer_time_interval = 60 * 60 # seconds
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!; output_path,
