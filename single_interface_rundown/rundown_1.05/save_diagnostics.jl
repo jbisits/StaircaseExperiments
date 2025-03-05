ee62b3e90faee10444e791ea9158abad98bdaffa
jbisits <jbisits@gmail.com>
2025-03-05 19:45:28 +1100

    More flexible save

--- single_interface_rundown/rundown_1.05/save_diagnostics.jl
+++ single_interface_rundown/rundown_1.05/save_diagnostics.jl
@@ -1,14 +1,16 @@
 using StaircaseShenanigans
 
-diagnostics_file = "diagnostics.jld2"
+initial_state = "step"
 
-linear_path = joinpath(@__DIR__, "R_rho_1.05", "step", "lineareos_single_interface_480min")
+diagnostics_file = initial_state*"/diagnostics.jld2"
+
+linear_path = joinpath(@__DIR__, "R_rho_1.05", initial_state, "lineareos_single_interface_480min")
 tracers = joinpath(linear_path, "tracers.nc")
 computed_output = joinpath(linear_path, "computed_output.nc")
 
 save_diagnostics!(diagnostics_file, tracers, computed_output, eos = "linear")
 
-nonlinear_path = joinpath(@__DIR__, "R_rho_1.05", "step", "nonlineareos_single_interface_480min")
+nonlinear_path = joinpath(@__DIR__, "R_rho_1.05", initial_state, "nonlineareos_single_interface_480min")
 tracers = joinpath(nonlinear_path, "tracers.nc")
 computed_output = joinpath(nonlinear_path, "computed_output.nc")
 
