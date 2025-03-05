2884951e5712ec2f49f07f3ebfc08649eb5a4bed
jbisits <jbisits@gmail.com>
2025-03-05 19:47:15 +1100

    Update save_diagnostics.jl

--- single_interface_rundown/rundown_1.05/save_diagnostics.jl
+++ single_interface_rundown/rundown_1.05/save_diagnostics.jl
@@ -4,13 +4,13 @@ initial_state = "step"
 
 diagnostics_file = initial_state*"/diagnostics.jld2"
 
-linear_path = joinpath(@__DIR__, "R_rho_1.05", initial_state, "lineareos_single_interface_480min")
+linear_path = joinpath(@__DIR__, initial_state, "lineareos_single_interface_480min")
 tracers = joinpath(linear_path, "tracers.nc")
 computed_output = joinpath(linear_path, "computed_output.nc")
 
 save_diagnostics!(diagnostics_file, tracers, computed_output, eos = "linear")
 
-nonlinear_path = joinpath(@__DIR__, "R_rho_1.05", initial_state, "nonlineareos_single_interface_480min")
+nonlinear_path = joinpath(@__DIR__, initial_state, "nonlineareos_single_interface_480min")
 tracers = joinpath(nonlinear_path, "tracers.nc")
 computed_output = joinpath(nonlinear_path, "computed_output.nc")
 
