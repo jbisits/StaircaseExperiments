c826b34098fc504ba58b768ee1555a8fb36588e0
jbisits <jbisits@gmail.com>
2025-03-07 08:42:33 +1100

    Add checkpointer for vdiff expts

--- single_interface_variable_diffusivity/linear_variable_diff.jl
+++ single_interface_variable_diffusivity/linear_variable_diff.jl
@@ -1,5 +1,7 @@
 using StaircaseShenanigans, GibbsSeaWater
 
+restart = false
+
 architecture = GPU()
 diffusivities = (ν = 1e-6, κ = (S = enhance_κₛ, T = enhance_κₜ),
                 parameters = (κₛ = 1e-9, κₜ = 1e-7, diff_change = 20, enhance = 10),
@@ -25,9 +27,14 @@ sdns = StaircaseDNS(model_setup, interface_ics, initial_noise = noise)
 ## Build simulation
 stop_time = 3 * 60 * 60 # seconds
 output_path = joinpath(@__DIR__, "variable_diff_$(round(interface_ics.R_ρ, digits = 2))")
+checkpointer_time_interval = 60 * 60 # seconds
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
-                                   save_vertical_velocities!; output_path)
+                                   save_vertical_velocities!; output_path,
+                                   checkpointer_time_interval,
+                                   overwrite_saved_output = restart)
 ## Run
+# simulation.stop_time = _ * 60 * 60 # update to pickup from a checkpoint
+pickup = restart ? false : readdir(simulation.output_writers[:checkpointer].dir, join = true)[1]
 run!(simulation)
 
 ## Compute density ratio
