6cdf3c2920d685eb70122d369a6152bcabc60f6c
jbisits <jbisits@gmail.com>
2025-03-08 18:43:38 +1100

    Update save_diagnostics.jl

--- single_interface_rundown/McDougall1981_25oct_1.26/save_diagnostics.jl
+++ single_interface_rundown/McDougall1981_25oct_1.26/save_diagnostics.jl
@@ -2,8 +2,8 @@ using StaircaseShenanigans
 
 diagnostics_file = "diagnostics.jld2"
 
-data_path = joinpath(@__DIR__, "nonlineareos_single_interface_480min")
+data_path = joinpath(@__DIR__, "nonlineareos_single_interface_240min")
 tracers = joinpath(data_path, "tracers.nc")
 computed_output = joinpath(data_path, "computed_output.nc")
 
-save_diagnostics!(diagnostics_file, tracers, computed_output, group = "diags")
+save_diagnostics!(diagnostics_file, tracers, computed_output, group = "diags_lessnoise")
