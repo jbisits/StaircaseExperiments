e9aeb62f7be59bd737dec669f721e55621c142e5
jbisits <jbisits@gmail.com>
2025-03-11 10:49:56 +1100

    All mcd expts

--- single_interface_rundown/McDougall1981_25oct_1.26/save_diagnostics.jl
+++ single_interface_rundown/McDougall1981_25oct_1.26/save_diagnostics.jl
@@ -1,9 +1,13 @@
 using StaircaseShenanigans
 
 diagnostics_file = "diagnostics.jld2"
+time_length = ("60min", "240min", "480min")
+groups = ("noise", "lessnoise", "nonoise")
 
-data_path = joinpath(@__DIR__, "nonlineareos_single_interface_240min")
-tracers = joinpath(data_path, "tracers.nc")
-computed_output = joinpath(data_path, "computed_output.nc")
+for (i, t) ∈ time_length
+    data_path = joinpath(@__DIR__, "nonlineareos_single_interface_"*t)
+    tracers = joinpath(data_path, "tracers.nc")
+    computed_output = joinpath(data_path, "computed_output.nc")
 
-save_diagnostics!(diagnostics_file, tracers, computed_output, group = "diags")
+    save_diagnostics!(diagnostics_file, tracers, computed_output, group = groups[i])
+end
