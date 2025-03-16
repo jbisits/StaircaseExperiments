a94deb2548927efaa00376d39cb33c2de01114df
jbisits <jbisits@gmail.com>
2025-03-17 09:10:49 +1100

    Use new defaults

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -27,12 +27,10 @@ stop_time = 1 * 60 * 60 # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 checkpointer_time_interval = 60 * 60 # seconds
-Δt = 1e-3
-max_Δt = 1e-2
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!; output_path,
                                    checkpointer_time_interval,
-                                   overwrite_saved_output = restart, max_Δt, Δt)
+                                   overwrite_saved_output = restart)
 ## Run
 # simulation.stop_time = 18 * 60 * 60 # update to pickup from a checkpoint
 pickup = restart ? false : readdir(simulation.output_writers[:checkpointer].dir, join = true)[1]
