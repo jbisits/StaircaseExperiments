4d6d61fa900aff1143e942b07811798fc4d72022
jbisits <jbisits@gmail.com>
2025-03-17 13:56:12 +1100

    Finer for both

--- single_interface_rundown/linear_rundown.jl
+++ single_interface_rundown/linear_rundown.jl
@@ -27,11 +27,12 @@ stop_time = 1 * 60 * 60 # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 checkpointer_time_interval = 60 * 60 # seconds
-max_Δt = 2e-2
+Δt = 1e-4
+max_Δt = 5e-3
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!; output_path,
                                    checkpointer_time_interval,
-                                   overwrite_saved_output = restart, max_Δt)
+                                   overwrite_saved_output = restart, max_Δt, Δt)
 ## Run
 # simulation.stop_time = 18 * 60 * 60 # update to pickup from a checkpoint
 pickup = restart ? false : readdir(simulation.output_writers[:checkpointer].dir, join = true)[1]
@@ -47,5 +48,5 @@ animation_path = simulation.output_writers[:computed_output].filepath[1:(reduced
 cd(animation_path)
 @info "Producing animations"
 using CairoMakie
-animate_density(simulation.output_writers[:computed_output].filepath, "σ")
-animate_tracers(simulation.output_writers[:tracers].filepath)
+animate_density(simulation.output_writers[:computed_output].filepath, "σ", xslice = 25, yslice = 25)
+animate_tracers(simulation.output_writers[:tracers].filepath, xslice = 25, yslice = 25)
