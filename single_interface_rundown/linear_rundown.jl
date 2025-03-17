e3a46606c84445f4d8d9cc36ef9a4ff8cd846cc6
jbisits <jbisits@gmail.com>
2025-03-17 12:59:11 +1100

    Linear as well?

--- single_interface_rundown/linear_rundown.jl
+++ single_interface_rundown/linear_rundown.jl
@@ -6,7 +6,7 @@ architecture = GPU()
 diffusivities = (ν=1e-5, κ=(S=1e-8, T=1e-6))
 domain_extent = (Lx=0.1, Ly=0.1, Lz=-0.5)
 domain_topology = (x = Periodic, y = Periodic, z = Bounded)
-resolution = (Nx=50, Ny=50, Nz=400)
+resolution = (Nx=50, Ny=50, Nz=250)
 ρ₀ = gsw_rho(34.57, 0.5, 0)
 eos = CustomLinearEquationOfState(-0.5, 34.6, reference_density = ρ₀)
 model_setup = (;architecture, diffusivities, domain_extent, domain_topology, resolution, eos)
@@ -17,7 +17,7 @@ depth_of_interface = -0.25
 salinity = [34.58, 34.70]
 temperature = [-1.5, 0.5]
 interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature)
-noise = NoiseAtDepth([-0.27, -0.23], TracerNoise(1e-5, 0.0))
+noise = NoiseAtDepth([-0.26, -0.24], VelocityNoise(0.0, 0.0, 1e-4))
 
 ## setup model
 sdns = StaircaseDNS(dns_model, interface_ics, initial_noise = noise)
@@ -27,10 +27,11 @@ stop_time = 1 * 60 * 60 # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 checkpointer_time_interval = 60 * 60 # seconds
+max_Δt = 2e-2
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!; output_path,
                                    checkpointer_time_interval,
-                                   overwrite_saved_output = restart)
+                                   overwrite_saved_output = restart, max_Δt)
 ## Run
 # simulation.stop_time = 18 * 60 * 60 # update to pickup from a checkpoint
 pickup = restart ? false : readdir(simulation.output_writers[:checkpointer].dir, join = true)[1]
