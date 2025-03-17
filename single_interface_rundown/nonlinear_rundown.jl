d568ca5ee960107b2ff3d5a904247db2dd61081d
jbisits <jbisits@gmail.com>
2025-03-17 14:33:28 +1100

    Smaller timestep still

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -27,8 +27,8 @@ stop_time = 1 * 60 * 60 # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 checkpointer_time_interval = 60 * 60 # seconds
-Δt = 1e-4
-max_Δt = 5e-3
+Δt = 1e-6
+max_Δt = 1e-4
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!; output_path,
                                    checkpointer_time_interval,
