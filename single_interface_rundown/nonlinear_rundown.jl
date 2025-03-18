8c4627616324f92260944d1775467a3d96b80902
jbisits <jbisits@gmail.com>
2025-03-19 08:38:11 +1100

    Smaller max time step, noise in all velocity fields, + interface thickness

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -16,9 +16,9 @@ dns_model = DNSModel(model_setup...)
 depth_of_interface = -0.25
 salinity = [34.58, 34.70]
 temperature = [-1.5, 0.5]
-interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature)
-                                    #interface_smoothing = TanhInterfaceThickness(0.01, 0.01))
-noise = NoiseAtDepth([-0.26, -0.24], VelocityNoise(0.0, 0.0, 1e-4))
+interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature,
+                                    interface_smoothing = TanhInterfaceThickness(0.01, 0.01))
+noise = NoiseAtDepth([-0.26, -0.24], VelocityNoise(1e-8, 1e-8, 1e-8))
 
 ## setup model
 sdns = StaircaseDNS(dns_model, interface_ics, initial_noise = noise)
@@ -28,7 +28,7 @@ stop_time = 1 * 60 * 60 # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 checkpointer_time_interval = 60 * 60 # seconds
-max_Δt = 5e-2
+max_Δt = 1e-2
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!; output_path,
                                    checkpointer_time_interval,
