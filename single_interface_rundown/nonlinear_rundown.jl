c967380a64caedd6146a91599ab0f69b605f3cdd
jbisits <jbisits@gmail.com>
2025-03-18 13:57:01 +1100

    Closer diffs

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -3,7 +3,7 @@ using StaircaseShenanigans, GibbsSeaWater
 restart = true
 
 architecture = GPU()
-diffusivities = (ν=1e-5, κ=(S=1e-8, T=1e-6))
+diffusivities = (ν=1e-5, κ=(S=1e-7, T=1e-6))
 domain_extent = (Lx=0.1, Ly=0.1, Lz=-0.5)
 domain_topology = (x = Periodic, y = Periodic, z = Bounded)
 resolution = (Nx=50, Ny=50, Nz=250)
@@ -17,6 +17,7 @@ depth_of_interface = -0.25
 salinity = [34.58, 34.70]
 temperature = [-1.5, 0.5]
 interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature)
+                                    #interface_smoothing = TanhInterfaceThickness(0.01, 0.01))
 noise = NoiseAtDepth([-0.26, -0.24], VelocityNoise(0.0, 0.0, 1e-4))
 
 ## setup model
@@ -27,13 +28,12 @@ stop_time = 1 * 60 * 60 # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 checkpointer_time_interval = 60 * 60 # seconds
-Δt = 1e-4
-max_Δt = 5e-3
+max_Δt = 5e-2
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!; output_path,
                                    checkpointer_time_interval,
                                    overwrite_saved_output = restart,
-                                    max_Δt, Δt)
+                                    max_Δt)
 ## Run
 # simulation.stop_time = 18 * 60 * 60 # update to pickup from a checkpoint
 pickup = restart ? false : readdir(simulation.output_writers[:checkpointer].dir, join = true)[1]
