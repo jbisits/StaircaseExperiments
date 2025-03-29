85c03315ab2476edb0717e474a4bf36c18192e0c
jbisits <jbisits@gmail.com>
2025-03-29 14:04:09 +1100

    Lower resolution

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -3,10 +3,10 @@ using StaircaseShenanigans, GibbsSeaWater
 restart = true
 
 architecture = GPU()
-diffusivities = (ν=1e-4, κ=(S=1.4e-7, T=1.4e-5))
+diffusivities = (ν=7e-4, κ=(S=7e-7, T=1e-5))
 domain_extent = (Lx=0.1, Ly=0.1, Lz=-0.5)
 domain_topology = (x = Periodic, y = Periodic, z = Bounded)
-resolution = (Nx=100, Ny=100, Nz=500)
+resolution = (Nx=60, Ny=60, Nz=250)
 ρ₀ = gsw_rho(34.7, 0.5, 0)
 eos = TEOS10EquationOfState(reference_density = ρ₀)
 model_setup = (;architecture, diffusivities, domain_extent, domain_topology, resolution, eos)
