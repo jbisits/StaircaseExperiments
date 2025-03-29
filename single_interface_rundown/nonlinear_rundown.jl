082a2c893e0e1d3d43ad3f82025fa5fdc54e9cf8
jbisits <jbisits@gmail.com>
2025-03-29 13:22:11 +1100

    Update nonlinear_rundown.jl

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -3,7 +3,7 @@ using StaircaseShenanigans, GibbsSeaWater
 restart = true
 
 architecture = GPU()
-diffusivities = (ν=1e-5, κ=(S=1.4e-8, T=1.4e-6))
+diffusivities = (ν=1e-4, κ=(S=1.4e-7, T=1.4e-5))
 domain_extent = (Lx=0.1, Ly=0.1, Lz=-0.5)
 domain_topology = (x = Periodic, y = Periodic, z = Bounded)
 resolution = (Nx=100, Ny=100, Nz=500)
@@ -27,7 +27,7 @@ stop_time = 1 * 60 * 60 # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 checkpointer_time_interval = 60 * 60 # seconds
-max_Δt = 1e-3
+max_Δt = 5e-2
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!; output_path,
                                    checkpointer_time_interval,
