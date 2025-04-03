a61b0a79959e76e69d3c55a7f9545a04ecee9dd5
jbisits <jbisits@gmail.com>
2025-04-03 17:23:18 +1100

    Smaller timestep + velocity noise

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -20,7 +20,7 @@ salinity = [34.58, 34.70]
 temperature = [-1.5, 0.5]
 interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature)
                                     # interface_smoothing = TanhInterfaceThickness(0.01, 0.01))
-initial_noise = NoiseAtDepth([depth_of_interface-0.02, depth_of_interface+0.02], TracerNoise(2e-4, 0.0))
+initial_noise = NoiseAtDepth([depth_of_interface-0.02, depth_of_interface+0.02], VelocityNoise(1e-5))
 ## setup model
 sdns = StaircaseDNS(dns_model, interface_ics; initial_noise)
 
@@ -30,7 +30,7 @@ initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 checkpointer_time_interval = 60 * 60 # seconds
 Δt = 1e-4
-max_Δt = 5e-3
+max_Δt = 2e-3
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!; output_path,
                                    checkpointer_time_interval,
