5750f2bf711fbe21aeead4a95482367655d29155
jbisits <jbisits@gmail.com>
2025-04-07 09:33:52 +1000

    Back to the one that worked + more frequent saving

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -3,10 +3,10 @@ using StaircaseShenanigans, GibbsSeaWater
 restart = true
 
 architecture = GPU()
-diffusivities = (ν=7e-4, κ=(S=1e-7, T=1e-5))
+diffusivities = (ν=1e-5, κ=(S=1.4e-8, T=1.4e-6))
 domain_extent = (Lx=0.07, Ly=0.07, Lz=-1.0)
 domain_topology = (x = Periodic, y = Periodic, z = Bounded)
-resolution = (Nx=40, Ny=40, Nz=600)
+resolution = (Nx=35, Ny=35, Nz=500)
 ρ₀ = gsw_rho(34.7, 0.5, 0)
 eos = TEOS10EquationOfState(reference_density = ρ₀)
 model_setup = (;architecture, diffusivities, domain_extent, domain_topology, resolution, eos)
@@ -14,13 +14,11 @@ dns_model = DNSModel(model_setup...; TD = VerticallyImplicitTimeDiscretization()
 
 ## Initial conditions
 depth_of_interface = -0.5
-# salinity = [34.63, 34.70]
-# temperature = [-0.5, 0.5]
 salinity = [34.58, 34.70]
 temperature = [-1.5, 0.5]
 interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature)
-                                    # interface_smoothing = TanhInterfaceThickness(0.01, 0.01))
-initial_noise = NoiseAtDepth([depth_of_interface-0.02, depth_of_interface+0.02], VelocityNoise(1e-4))
+
+initial_noise = NoiseAtDepth([depth_of_interface-0.02, depth_of_interface+0.02], VelocityNoise(1e-5))
 ## setup model
 sdns = StaircaseDNS(dns_model, interface_ics; initial_noise)
 
@@ -28,11 +26,14 @@ sdns = StaircaseDNS(dns_model, interface_ics; initial_noise)
 stop_time = 1 * 60 * 60 # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
+save_schedule = 15
 checkpointer_time_interval = 60 * 60 # seconds
 Δt = 1e-4
 max_Δt = 2e-3
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
-                                   save_vertical_velocities!; output_path,
+                                   save_vertical_velocities!;
+                                   output_path,
+                                   save_schedule,
                                    checkpointer_time_interval,
                                    overwrite_saved_output = restart,
                                    max_Δt, Δt)
