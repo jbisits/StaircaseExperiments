5768663eb28ce756ac397400d2a41f4472e5fc5a
jbisits <jbisits@gmail.com>
2025-04-15 13:24:25 +1000

    Different noise magnitude for S and T

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -19,7 +19,7 @@ temperature = [-1.5, 0.5]
 interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature)
 
 # initial_noise = NoiseAtDepth([depth_of_interface-0.02, depth_of_interface+0.02], TracerNoise(2e-4, 0.0))
-initial_noise = (velocities = VelocityNoise(1e-3), tracers = TracerNoise(1e-3))
+initial_noise = (velocities = VelocityNoise(1e-3), tracers = TracerNoise(1e-4, 1e-2))
 ## setup model
 sdns = StaircaseDNS(dns_model, interface_ics; initial_noise)
 
