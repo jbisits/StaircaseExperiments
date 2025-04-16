027af8f841e582caada55b4569af815f3b010d07
jbisits <jbisits@gmail.com>
2025-04-16 14:39:24 +1000

    Shorter run + smaller \nu

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -5,7 +5,7 @@ restart = true
 architecture = GPU()
 Pr = 7   # Prandtl
 τ = 0.05 # diff ratio
-ν = 5e-6 # set this get the others
+ν = 2.5e-6 # set this get the others
 κₜ, κₛ = κₛ_and_κₜ_from_ν(ν; τ, Pr)
 Sc = ν / κₛ
 diffusivities = (;ν, κ=(S=κₛ, T=κₜ))
@@ -29,7 +29,7 @@ initial_noise = (velocities = VelocityNoise(1e-2), tracers = TracerNoise(1e-4, 1
 sdns = StaircaseDNS(dns_model, interface_ics; initial_noise)
 
 ## Build simulation
-stop_time = Int(1 * 60 * 60) # seconds
+stop_time = Int(0.5 * 60 * 60) # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 save_schedule = 30
