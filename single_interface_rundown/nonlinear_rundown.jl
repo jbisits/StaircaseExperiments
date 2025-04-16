fb01cfae38fc05eb17bb5ca2b338a7ddaa1c17d8
jbisits <jbisits@gmail.com>
2025-04-16 17:21:15 +1000

    Larger Prandtl + save diags on sim

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -4,7 +4,7 @@ restart = true
 
 architecture = GPU()
 Pr = 7   # Prandtl
-τ = 0.05 # diff ratio
+τ = 0.1 # diff ratio
 ν = 2.5e-6 # set this get the others
 κₜ, κₛ = κₛ_and_κₜ_from_ν(ν; τ, Pr)
 Sc = ν / κₛ
@@ -29,7 +29,7 @@ initial_noise = (velocities = VelocityNoise(1e-2), tracers = TracerNoise(1e-4, 1
 sdns = StaircaseDNS(dns_model, interface_ics; initial_noise)
 
 ## Build simulation
-stop_time = Int(0.5 * 60 * 60) # seconds
+stop_time = Int(1 * 60 * 60) # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 save_schedule = 30
@@ -55,9 +55,19 @@ compute_R_ρ!(simulation.output_writers[:computed_output].filepath,
 
 ## Produce animations
 reduced_path = findlast('/', simulation.output_writers[:computed_output].filepath)
-animation_path = simulation.output_writers[:computed_output].filepath[1:(reduced_path-1)]
-cd(animation_path)
+output_path = simulation.output_writers[:computed_output].filepath[1:(reduced_path-1)]
+cd(output_path)
 @info "Producing animations"
 using CairoMakie
 animate_density(simulation.output_writers[:computed_output].filepath, "σ", xslice = 25, yslice = 25)
 animate_tracers(simulation.output_writers[:tracers].filepath, xslice = 25, yslice = 25)
+
+## compute diagnostics
+diags = "diagnostics.jld2"
+if isfile(diags)
+    rm(diags)
+end
+save_diagnostics!(diags,
+                  simulation.output_writers[:tracers].filepath,
+                  simulation.output_writers[:computed_output].filepath,
+                  simulation.output_writers[:velocities].filepath)
