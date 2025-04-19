a549f3a2174fd998123187616d80b99987b7f39b
jbisits <jbisits@gmail.com>
2025-04-19 20:12:17 +1000

    Larger time

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -34,8 +34,8 @@ initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 save_schedule = 60
 checkpointer_time_interval = 60 * 60 # seconds
-Δt = 1e-4
-max_Δt = 2e-3
+Δt = 1e-3
+max_Δt = 1e-2
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!;
                                    output_path,
