8378e09e60d2fce3f32c224a9233e47781d3858e
jbisits <jbisits@gmail.com>
2025-04-21 21:07:38 +1000

    Run for longer

--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -4,11 +4,9 @@ restart = true
 
 architecture = GPU()
 Pr = 7   # Prandtl
-τ = 0.01 # τ = 0.05 # diff ratio
-ν = 6.5e-6 # ν = 2.5e-6 # set this get the others
-κₜ, κₛ = κₛ_and_κₜ_from_ν(ν; τ, Pr)
-Sc = ν / κₛ
-diffusivities = (;ν, κ=(S=κₛ, T=κₜ))
+τ = 0.05 # diff ratio
+ν = 2.5e-6 # set this get the others
+diffusivities = diffusivities_from_ν(ν; τ, Pr)
 domain_extent = (Lx=0.05, Ly=0.05, Lz=-1.0)
 domain_topology = (x = Periodic, y = Periodic, z = Bounded)
 resolution = (Nx=50, Ny=50, Nz=1000)
@@ -29,13 +27,13 @@ initial_noise = (velocities = VelocityNoise(1e-2), tracers = TracerNoise(1e-4, 1
 sdns = StaircaseDNS(dns_model, interface_ics; initial_noise)
 
 ## Build simulation
-stop_time = Int(0.5 * 60 * 60) # seconds
+stop_time = Int(5 * 60 * 60) # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 save_schedule = 60
 checkpointer_time_interval = 60 * 60 # seconds
 Δt = 1e-3
-max_Δt = 1e-2
+max_Δt = 7e-2
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!;
                                    output_path,
