0640c46e974e9a6b9a8dafd53f30e6e54fd0428e
jbisits <jbisits@gmail.com>
2025-04-23 11:26:34 +1000

    Long one

--- CPUExperiments/single_interface_rundown.jl
+++ CPUExperiments/single_interface_rundown.jl
@@ -50,9 +50,10 @@ animate_tracers(simulation.output_writers[:tracers].filepath, xslice = 2, yslice
 
 
 # mwe for differing diffusivities
-using Oceananigans
+using Oceananigans, CairoMakie
 
-grid = RectilinearGrid(architecture,
+## Setting S diffusivity first
+grid = RectilinearGrid(CPU(),
                        topology = (Periodic, Flat, Bounded),
                        size = (5, 50),
                        x = (-0.01/2, 0.01/2),
@@ -64,9 +65,15 @@ tracers = (:S, :T)
 closure = ScalarDiffusivity(; ν=1e-4, κ=(S=1e-7, T=1e-5))
 
 model = NonhydrostaticModel(; grid, buoyancy, tracers, closure)
-# set!(model, S = , T = ) do this when you get a chance
+Tᵤ, Tₗ = -1.5, 0.5
+T₀(x, z) = z <= -0.5 ? Tᵤ : Tₗ
+Sᵤ, Sₗ = 34.58, 34.57
+S₀(x, z) = z <= -0.5 ? Sᵤ : Sₗ
+set!(model, S = S₀, T = T₀)
 
 simulation = Simulation(model, Δt=0.1, stop_time=60*60)
+wizard = TimeStepWizard()
+simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
 
 tracers = Dict("S" => model.tracers.S, "T" => model.tracers.T)
 simulation.output_writers[:tracers] = JLD2Writer(model, tracers;
@@ -77,5 +84,64 @@ run!(simulation)
 
 S = FieldTimeSeries("tracers_κₛ_κₜ.jld2", "S")
 T = FieldTimeSeries("tracers_κₛ_κₜ.jld2", "T")
+x = xnodes(S.grid, Center())
+z = znodes(S.grid, Center())
 
-heatmap(interior(S, :, 1, :, 1))
+fig = Figure(size = (1000, 1000))
+axS₀ = Axis(fig[1, 1], ylabel = "z", title = "Salinity t = 0")
+heatmap!(axS₀, x, z, interior(S, :, 1, :, 1), colormap = :haline)
+axT₀ = Axis(fig[1, 2], ylabel = "z", title = "Temperature t = 0")
+heatmap!(axT₀, x, z, interior(T, :, 1, :, 1), colormap = :thermal)
+axSend = Axis(fig[2, 1], xlabel = "x", ylabel = "z", title = "Salinity t = 61")
+heatmap!(axSend, x, z, interior(S, :, 1, :, 61), colormap = :haline)
+axTend = Axis(fig[2, 2], xlabel = "x", ylabel = "z", title = "Temperature t = 61")
+heatmap!(axTend, x, z, interior(T, :, 1, :, 61), colormap = :thermal)
+fig
+save("Sdiff_set_first.png", fig)
+
+## Setting T diffusivity first
+grid = RectilinearGrid(CPU(),
+                       topology = (Periodic, Flat, Bounded),
+                       size = (5, 50),
+                       x = (-0.01/2, 0.01/2),
+                       z = (-1, 0))
+
+buoyancy = SeawaterBuoyancy()
+tracers = (:S, :T)
+
+closure = ScalarDiffusivity(; ν=1e-4, κ=(T=1e-5, S=1e-7)) # T set first
+
+model = NonhydrostaticModel(; grid, buoyancy, tracers, closure)
+Tᵤ, Tₗ = -1.5, 0.5
+T₀(x, z) = z <= -0.5 ? Tᵤ : Tₗ
+Sᵤ, Sₗ = 34.58, 34.57
+S₀(x, z) = z <= -0.5 ? Sᵤ : Sₗ
+set!(model, S = S₀, T = T₀)
+
+simulation = Simulation(model, Δt=0.1, stop_time=60*60)
+wizard = TimeStepWizard()
+simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
+
+tracers = Dict("S" => model.tracers.S, "T" => model.tracers.T)
+simulation.output_writers[:tracers] = JLD2Writer(model, tracers;
+                                                filename = "tracers_κₜ_κₛ",
+                                                schedule = TimeInterval(60))
+
+run!(simulation)
+
+S = FieldTimeSeries("tracers_κₜ_κₛ.jld2", "S")
+T = FieldTimeSeries("tracers_κₜ_κₛ.jld2", "T")
+x = xnodes(S.grid, Center())
+z = znodes(S.grid, Center())
+
+fig = Figure(size = (1000, 1000))
+axS₀ = Axis(fig[1, 1], ylabel = "z", title = "Salinity t = 0")
+heatmap!(axS₀, x, z, interior(S, :, 1, :, 1), colormap = :haline)
+axT₀ = Axis(fig[1, 2], ylabel = "z", title = "Temperature t = 0")
+heatmap!(axT₀, x, z, interior(T, :, 1, :, 1), colormap = :thermal)
+axSend = Axis(fig[2, 1], xlabel = "x", ylabel = "z", title = "Salinity t = 61")
+heatmap!(axSend, x, z, interior(S, :, 1, :, 61), colormap = :haline)
+axTend = Axis(fig[2, 2], xlabel = "x", ylabel = "z", title = "Temperature t = 61")
+heatmap!(axTend, x, z, interior(T, :, 1, :, 61), colormap = :thermal)
+fig
+save("Tdiff_set_first.png", fig)
--- single_interface_rundown/nonlinear_rundown.jl
+++ single_interface_rundown/nonlinear_rundown.jl
@@ -1,6 +1,6 @@
 using StaircaseShenanigans, GibbsSeaWater
 
-restart = false
+restart = true
 
 architecture = GPU()
 Pr = 7   # Prandtl
@@ -28,7 +28,7 @@ initial_noise = (velocities = VelocityNoise(1e-2), tracers = TracerNoise(1e-4, 1
 sdns = StaircaseDNS(dns_model, interface_ics; initial_noise)
 
 ## Build simulation
-stop_time = Int(1 * 60 * 60) # seconds
+stop_time = Int(7 * 60 * 60) # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 save_schedule = 60
