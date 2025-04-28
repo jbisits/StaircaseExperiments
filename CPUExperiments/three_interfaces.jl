ee0549b42700b3cb79a97bf8a6ab926a2acd7504
jbisits <jbisits@gmail.com>
2025-04-28 11:12:38 +1000

    Staircases - here we go!

---
+++ CPUExperiments/three_interfaces.jl
@@ -0,0 +1,35 @@
+using StaircaseShenanigans, GibbsSeaWater, CairoMakie
+
+architecture = CPU()
+Pr = 7   # Prandtl
+τ = 0.05 # diff ratio
+ν = 2.5e-6 # set this get the others
+diffusivities = diffusivities_from_ν(ν; τ, Pr)
+domain_extent = (Lx=0.05, Ly=0.05, Lz=-1.0)
+domain_topology = (x = Periodic, y = Periodic, z = Bounded)
+resolution = (Nx=5, Ny=5, Nz=100)
+ρ₀ = gsw_rho(34.7, 0.5, 0)
+eos = TEOS10EquationOfState(reference_density = ρ₀)
+ρ₀ = gsw_rho(34.57, 0.5, 0)
+eos = CustomLinearEquationOfState(-0.5, 34.6, reference_density = ρ₀)
+model_setup = (;architecture, diffusivities, domain_extent, domain_topology, resolution, eos)
+model = DNSModel(model_setup...; TD = VerticallyImplicitTimeDiscretization())
+
+number_of_interfaces = 3
+depth_of_interfaces = [-0.25, -0.5, -0.75]
+#### nonlinear eos
+salinity = [34.58, 34.61, 34.65, 34.7]
+temperature = [-1.51, -0.87, -0.19, 0.51]
+#### linear eos
+salinity = [34.58, 34.61, 34.65, 34.7]
+temperature = [-1.5, -1.0, -0.33, 0.5]
+staircase_ics = StaircaseICs(model, number_of_interfaces, depth_of_interfaces, salinity, temperature)
+
+initial_noise = (velocities = VelocityNoise(1e-2), tracers = TracerNoise(1e-4, 1e-2))
+
+## setup model
+sdns = StaircaseDNS(model, staircase_ics; initial_noise)
+
+visualise_initial_conditions(sdns, 5, 5)
+
+visualise_initial_density(sdns, 5, 5, 0)
---
+++ staircase_rundown/nonlinear_three_interfaces.jl
@@ -0,0 +1,61 @@
+using StaircaseShenanigans, GibbsSeaWater
+
+restart = true
+
+architecture = GPU()
+Pr = 7   # Prandtl
+τ = 0.05 # diff ratio
+ν = 2.5e-6 # set this get the others
+diffusivities = diffusivities_from_ν(ν; τ, Pr)
+domain_extent = (Lx=0.05, Ly=0.05, Lz=-1.0)
+domain_topology = (x = Periodic, y = Periodic, z = Bounded)
+resolution = (Nx=50, Ny=50, Nz=500)
+ρ₀ = gsw_rho(34.7, 0.5, 0)
+eos = TEOS10EquationOfState(reference_density = ρ₀)
+model_setup = (;architecture, diffusivities, domain_extent, domain_topology, resolution, eos)
+model = DNSModel(model_setup...; TD = VerticallyImplicitTimeDiscretization())
+
+number_of_interfaces = 3
+depth_of_interfaces = [-0.25, -0.5, -0.75]
+salinity = [34.58, 34.61, 34.65, 34.7]
+temperature = [-1.51, -0.87, -0.19, 0.51]
+staircase_ics = StaircaseICs(model, number_of_interfaces, depth_of_interfaces, salinity, temperature)
+
+initial_noise = (velocities = VelocityNoise(1e-2), tracers = TracerNoise(1e-4, 1e-2))
+
+## setup model
+sdns = StaircaseDNS(model, staircase_ics; initial_noise)
+
+## Build simulation
+stop_time = Int(4 * 60 * 60) # seconds
+initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
+output_path = joinpath(@__DIR__, "rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
+save_schedule = 60
+checkpointer_time_interval = 60 * 60 # seconds
+Δt = 1e-3
+max_Δt = 7e-2
+simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
+                                   save_vertical_velocities!;
+                                   output_path,
+                                   save_schedule,
+                                   checkpointer_time_interval,
+                                   overwrite_saved_output = restart,
+                                   max_Δt,
+                                   Δt)
+## Run
+restart ? nothing : simulation.stop_time = Int(10 * 60 * 60) # update to pickup from a checkpoint
+pickup = restart ? false : readdir(simulation.output_writers[:checkpointer].dir, join = true)[1]
+run!(simulation; pickup)
+
+## Compute density ratio
+compute_R_ρ!(simulation.output_writers[:computed_output].filepath,
+             simulation.output_writers[:tracers].filepath, eos)
+
+## Produce animations
+reduced_path = findlast('/', simulation.output_writers[:computed_output].filepath)
+output_path = simulation.output_writers[:computed_output].filepath[1:(reduced_path-1)]
+cd(output_path)
+@info "Producing animations"
+using CairoMakie
+animate_density(simulation.output_writers[:computed_output].filepath, "σ", xslice = 25, yslice = 25)
+animate_tracers(simulation.output_writers[:tracers].filepath, xslice = 25, yslice = 25)
