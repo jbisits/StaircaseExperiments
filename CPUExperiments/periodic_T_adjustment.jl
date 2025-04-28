6a009e87b55e7ab9077ab6ede2553ef0197f1368
jbisits <jbisits@gmail.com>
2025-04-28 15:01:40 +1000

    Update variable setting

--- CPUExperiments/periodic_T_adjustment.jl
+++ CPUExperiments/periodic_T_adjustment.jl
@@ -2,14 +2,14 @@ using StaircaseShenanigans, GibbsSeaWater
 
 restart = true
 
-architecture = GPU()
+architecture = CPU()
 Pr = 7   # Prandtl
 τ = 0.05 # diff ratio
 ν = 2.5e-6 # set this get the others
 diffusivities = diffusivities_from_ν(ν; τ, Pr)
 domain_extent = (Lx=0.05, Ly=0.05, Lz=-1.0)
 domain_topology = (x = Periodic, y = Periodic, z = Bounded)
-resolution = (Nx=50, Ny=50, Nz=500)
+resolution = (Nx=5, Ny=5, Nz=100)
 ρ₀ = gsw_rho(34.7, 0.5, 0)
 eos = TEOS10EquationOfState(reference_density = ρ₀)
 model_setup = (;architecture, diffusivities, domain_extent, domain_topology, resolution, eos)
@@ -46,28 +46,24 @@ background_fields = (S = S_bf, T = T_bf)
 interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature,
                                     background_state = background_fields)
 
-# interface_ics = SingleInterfaceICs(eos, depth_of_interface, salinity, temperature,
-#                                     background_state = BackgroundLinear())
 initial_noise = (velocities = VelocityNoise(1e-2), tracers = TracerNoise(1e-4, 1e-2))
 
 ## setup model
 sdns = StaircaseDNS(model_setup, interface_ics, initial_noise)
 
 ## Build simulation
-stop_time = Int(4 * 60 * 60) # seconds
+stop_time = Int(1 * 60 * 60) # seconds
 initial_state = interface_ics.interface_smoothing isa TanhInterfaceThickness ?  "tanh" : "step"
 output_path = joinpath(@__DIR__, "dns_rundown_$(round(interface_ics.R_ρ, digits = 2))", initial_state)
 save_schedule = 60
 checkpointer_time_interval = 60 * 60 # seconds
-Δt = 1e-3
-max_Δt = 7e-2
+Δt = 1e-1
 simulation = SDNS_simulation_setup(sdns, stop_time, save_computed_output!,
                                    save_vertical_velocities!;
                                    output_path,
                                    save_schedule,
                                    checkpointer_time_interval,
                                    overwrite_saved_output = restart,
-                                   max_Δt,
                                    Δt)
 ## Run
 restart ? nothing : simulation.stop_time = Int(10 * 60 * 60) # update to pickup from a checkpoint
@@ -84,5 +80,5 @@ output_path = simulation.output_writers[:computed_output].filepath[1:(reduced_pa
 cd(output_path)
 @info "Producing animations"
 using CairoMakie
-animate_density(simulation.output_writers[:computed_output].filepath, "σ", xslice = 25, yslice = 25)
-animate_tracers(simulation.output_writers[:tracers].filepath, xslice = 25, yslice = 25)
+animate_density(simulation.output_writers[:computed_output].filepath, "σ", xslice = 2, yslice = 2)
+animate_tracers(simulation.output_writers[:tracers].filepath, xslice = 2, yslice = 2)
