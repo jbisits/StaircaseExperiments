258ae09127343b0b70393e3d21f91f26f3902507
jbisits <jbisits@gmail.com>
2025-05-09 20:58:52 +1000

    Increase fluxes for si bcs

--- single_interface_fluxbcs/linear_fluxbcs.jl
+++ single_interface_fluxbcs/linear_fluxbcs.jl
@@ -16,9 +16,9 @@ model_setup = (;architecture, diffusivities, domain_extent, domain_topology, res
 # bcs from a rundown model and are an approximation/test to see if can simulate
 # effect of interfaces either side.
 Jᵀ = 1.5e-5
-T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(0.75*Jᵀ), bottom = FluxBoundaryCondition(0.75*Jᵀ))
+T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(0.9*Jᵀ), bottom = FluxBoundaryCondition(0.9*Jᵀ))
 Jˢ = 2e-7
-S_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(0.75*Jˢ), bottom = FluxBoundaryCondition(0.75*Jˢ))
+S_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(0.9*Jˢ), bottom = FluxBoundaryCondition(0.9*Jˢ))
 boundary_conditions = (T=T_bcs, S=S_bcs)
 dns_model = DNSModel(model_setup...; boundary_conditions, TD = VerticallyImplicitTimeDiscretization())
 
@@ -80,16 +80,16 @@ end
 close(ds)
 
 ## local plot of figure
-using JLD2, CairoMakie
-output_path = joinpath(@__DIR__, "R_rho_1.05/linear/")
-data = joinpath(output_path, "R_rho.jld2")
-f = jldopen(data)
-R_ρ = f["R_ρ"]
-close(f)
+# using JLD2, CairoMakie
+# output_path = joinpath(@__DIR__, "R_rho_1.05/linear/")
+# data = joinpath(output_path, "R_rho.jld2")
+# f = jldopen(data)
+# R_ρ = f["R_ρ"]
+# close(f)
 
-fig, ax = lines(R_ρ)
-ax.title = "R_ρ with fluxbcs linear eos"
-ax.xlabel = "time (mins)"
-ax.ylabel = "R_ρ"
-fig
-save(joinpath(output_path, "R_rho.png"), fig)
+# fig, ax = lines(R_ρ)
+# ax.title = "R_ρ with fluxbcs linear eos"
+# ax.xlabel = "time (mins)"
+# ax.ylabel = "R_ρ"
+# fig
+# save(joinpath(output_path, "R_rho.png"), fig)
