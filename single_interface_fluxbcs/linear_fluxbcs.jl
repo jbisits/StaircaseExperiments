769e24b482e57ced3fff2d67b73f11ae3a7e4c12
jbisits <jbisits@gmail.com>
2025-05-17 17:25:31 +1000

    Larger still

--- single_interface_fluxbcs/linear_fluxbcs.jl
+++ single_interface_fluxbcs/linear_fluxbcs.jl
@@ -15,7 +15,7 @@ eos = CustomLinearEquationOfState(-0.5, 34.6, reference_density = ρ₀)
 model_setup = (;architecture, diffusivities, domain_extent, domain_topology, resolution, eos)
 # bcs from a rundown model and are an approximation/test to see if can simulate
 # effect of interfaces either side.
-Jᵀ = 2 * 1.5e-5
+Jᵀ = 3 * 1.5e-5
 T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Jᵀ), bottom = FluxBoundaryCondition(Jᵀ))
 Jˢ = 2e-7
 S_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Jˢ), bottom = FluxBoundaryCondition(Jˢ))
