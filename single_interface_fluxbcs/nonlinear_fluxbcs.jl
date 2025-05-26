aace1978f888c64330685bc32793fcfa19da6632
jbisits <jbisits@gmail.com>
2025-05-26 12:53:38 +1000

    Reduce S bc flux

--- single_interface_fluxbcs/nonlinear_fluxbcs.jl
+++ single_interface_fluxbcs/nonlinear_fluxbcs.jl
@@ -18,7 +18,7 @@ model_setup = (;architecture, diffusivities, domain_extent, domain_topology, res
 Jᵀ = 2.2e-5
 Jˢ = 4.9e-7
 T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(0.5*Jᵀ), bottom = FluxBoundaryCondition(Jᵀ))
-S_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Jˢ), bottom = FluxBoundaryCondition(Jˢ))
+S_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(0.5*Jˢ), bottom = FluxBoundaryCondition(Jˢ))
 boundary_conditions = (T=T_bcs, S=S_bcs)
 dns_model = DNSModel(model_setup...; boundary_conditions, TD = VerticallyImplicitTimeDiscretization())
 
