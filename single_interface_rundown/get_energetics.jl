927df22cbd1bbcf42111039bb7b69e4e35f90709
jbisits <jbisits@gmail.com>
2025-05-30 15:16:22 +1000

    speling

--- single_interface_rundown/get_energetics.jl
+++ single_interface_rundown/get_energetics.jl
@@ -5,10 +5,10 @@ output_path = joinpath(@__DIR__, "dns_rundown_1.05/step/lineareos_single_interfa
 co = joinpath(output_path, "computed_output.nc")
 tracers = joinpath(output_path, "tracers.nc")
 linear_energetics = joinpath(output_path, "energetics.jld2")
-potential_and_background_potential_energy!(linear_energetic, co, tracers, "")
+potential_and_background_potential_energy!(linear_energetics, co, tracers, "")
 
 output_path = joinpath(@__DIR__, "dns_rundown_1.05/step/nonlineareos_single_interface_60min")
 co = joinpath(output_path, "computed_output.nc")
 tracers = joinpath(output_path, "tracers.nc")
 nlinear_energetics = joinpath(output_path, "energetics.jld2")
-potential_and_background_potential_energy!(nlinear_energetic, co, tracers, "")
+potential_and_background_potential_energy!(nlinear_energetics, co, tracers, "")
