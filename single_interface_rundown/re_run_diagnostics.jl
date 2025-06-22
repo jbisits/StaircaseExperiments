a1b08aaa4e313e278ec73ec65fd42a6cc303939e
jbisits <jbisits@gmail.com>
2025-06-23 09:41:59 +1000

    Re-run on dns simulations

--- single_interface_rundown/re_run_diagnostics.jl
+++ single_interface_rundown/re_run_diagnostics.jl
@@ -1,7 +1,29 @@
 using StaircaseShenanigans, CairoMakie, GibbsSeaWater
 
-# update output path to where files are located
-output_path = joinpath(@__DIR__, "dns_rundown_1.05/step/nonlineareos_single_interface_60min")
+# Non-linear eos
+output_path = joinpath(@__DIR__, "dns_rundown_1.05_dT_-1.0/step/nonlineareos_single_interface_120min")
+computed_output = joinpath(output_path, "computed_output.nc")
+tracers = joinpath(output_path, "tracers.nc")
+velocities = joinpath(output_path, "velocities.nc")
+
+@info "Re-producing animations"
+animate_density(computed_output, "σ",
+                xslice = 17, yslice = 17, density_limit_adjustment = 0.04)
+animate_tracers(tracers, xslice = 17, yslice = 17,
+                S_limit_adjustment = 0.025,
+                Θ_limit_adjustment = 0.5)
+animate_vertical_velocity(velocities, xslice = 17, yslice = 17)
+
+@info "Re-computing diagnostics"
+diags = joinpath(output_path, "step_diagnostics.jld2")
+if isfile(diags)
+    rm(diags)
+end
+save_diagnostics!(diags, tracers, computed_output, velocities)
+
+
+# Linear eos
+output_path = joinpath(@__DIR__, "dns_rundown_1.05_dT_-1.0/step/lineareos_single_interface_120min")
 computed_output = joinpath(output_path, "computed_output.nc")
 tracers = joinpath(output_path, "tracers.nc")
 velocities = joinpath(output_path, "velocities.nc")
