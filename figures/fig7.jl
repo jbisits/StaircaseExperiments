0e682f3929b37f50e3606108dc712082556dcedf
jbisits <jbisits@gmail.com>
2026-04-28 15:20:07 +1000

    Change panel order in figure 7

--- figures/fig7.jl
+++ figures/fig7.jl
@@ -1,13 +1,37 @@
 include("plotting_env_and_load.jl")
 ## Figure seven
+fig = Figure(size = (800, 1000))
+# density ratio and salinity midpoint
+ax_interface = Axis(fig[1, 1],
+                    title = "(a) Salinity interface height",
+                    xlabel = "time (min)",
+                    ylabel = L"$z*$ (m)")
+ax_R_rho = Axis(fig[2, 1],
+                title = "(b) Density ratio",
+                xlabel = "time ( min)",
+                ylabel = L"R_{\rho}")
+
+for (i, file) ∈ enumerate(files)
+    jldopen(file) do f
+        t = f["dims"]["time"][1:ts_length]
+        z✶ = f["dims"]["z✶"][f["S_interface_idx"]][1:ts_length]
+        Rᵨ = f["R_ρ"][1:ts_length]
+        ls = i % 2 == 0 ? :dash : :solid
+        lines!(ax_interface, t ./ 60, z✶, label = all_labels[i], linestyle = ls)
+        lines!(ax_R_rho, t ./ 60, Rᵨ, label = all_labels[i], linestyle = ls)
+    end
+end
+hidexdecorations!(ax_R_rho, grid = false, ticks = false)
+hidexdecorations!(ax_interface, grid = false, ticks = false)
+linkxaxes!(ax_R_rho, ax_interface)
+
+# energetics
 files = (l_R_ρ_105_dT2_diagnostics, nl_R_ρ_105_dT2_diagnostics,
          l_R_ρ_105_dT1_diagnostics, nl_R_ρ_105_dT1_diagnostics,
          l_R_ρ_105_dT05_diagnostics, nl_R_ρ_105_dT05_diagnostics)
-# energetics
-fig = Figure(size = (800, 1000))
-ax = [Axis(fig[i, 1], xlabel = "time (min)", ylabel = L"\mathcal{APE}") for i ∈ 1:2]
-ax[1].title = "(a) Available potential energy"
-ax[2].title = "(b) Time derivateive of APE, experiments I and IV"
+ax = [Axis(fig[i, 1], xlabel = "time (min)", ylabel = L"\mathcal{APE}") for i ∈ 3:4]
+ax[1].title = "(c) Available potential energy"
+ax[2].title = "(d) Time derivateive of APE, experiments I and IV"
 ts_length = 240
 j = 1
 linestyles = (:solid, :dash)
@@ -34,31 +58,8 @@ for (i, f) ∈ enumerate(files)
     end
 end
 hidexdecorations!(ax[1], grid = false, ticks = false)
-hidexdecorations!(ax[2], grid = false, ticks = false)
 linkxaxes!(ax[1], ax[2])
 
-# density ratio and salinity midpoint
-ax_R_rho = Axis(fig[3, 1],
-                title = "(c) Density ratio",
-                xlabel = "time ( min)",
-                ylabel = L"R_{\rho}")
-ax_interface = Axis(fig[4, 1],
-                    title = "(d) Salinity interface height",
-                    xlabel = "time (min)",
-                    ylabel = L"$z*$ (m)")
-
-for (i, file) ∈ enumerate(files)
-    jldopen(file) do f
-        t = f["dims"]["time"][1:ts_length]
-        z✶ = f["dims"]["z✶"][f["S_interface_idx"]][1:ts_length]
-        Rᵨ = f["R_ρ"][1:ts_length]
-        ls = i % 2 == 0 ? :dash : :solid
-        lines!(ax_interface, t ./ 60, z✶, label = all_labels[i], linestyle = ls)
-        lines!(ax_R_rho, t ./ 60, Rᵨ, label = all_labels[i], linestyle = ls)
-    end
-end
-hidexdecorations!(ax_R_rho, grid = false, ticks = false)
-linkxaxes!(ax_R_rho, ax_interface)
 Legend(fig[5, 1], ax_R_rho, "Experiment", orientation = :horizontal, nbanks = 2)
 ##
 save("fig7.png", fig)
