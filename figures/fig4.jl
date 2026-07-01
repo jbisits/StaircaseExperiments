7091de121fecee6e2fac7d9db15f64b091e32ba6
jbisits <jbisits@gmail.com>
2026-07-01 14:06:28 +1000

    figure updates based on reviews

--- figures/fig4.jl
+++ figures/fig4.jl
@@ -1,8 +1,8 @@
 include("plotting_env_and_load.jl")
 ## Figure four
 # Density asymmetry heatmap
-Sᵤ_range = range(33.54, Sₗ, length = 400)
-Θᵤ_range = range(-1.6, Θₗ, length = 400)
+Sᵤ_range = range(33.54, Sₗ, length=400)
+Θᵤ_range = range(-1.6, Θₗ, length=400)
 Rᵨ_leos = Array{Float64}(undef, length(Θᵤ_range), length(Sᵤ_range))
 Rᵨ_nleos = similar(Rᵨ_leos)
 σ₀_nonlinear_max = similar(Rᵨ_leos)
@@ -15,7 +15,7 @@ Rᵨ_nleos = similar(Rᵨ_leos)
 σ₀ˡ_leos = total_density(Θₗ, Sₗ, 0, leos)
 τ = 0.1
 Δρ′_nlinear = similar(Rᵨ_leos)
-δ(C) = 1 + 2*C + 2*sqrt(C^2 + C)
+δ(C) = 1 + 2 * C + 2 * sqrt(C^2 + C)
 δ_leos = similar(Rᵨ_leos)
 δ_nleos = similar(Rᵨ_leos)
 for (j, _Sᵤ) ∈ enumerate(Sᵤ_range)
@@ -45,7 +45,7 @@ for (j, _Sᵤ) ∈ enumerate(Sᵤ_range)
         σ₀ᵘ_leos[i, j] = total_density(_Θᵤ, _Sᵤ, 0, leos)
 
         slope = _ΔΘ / _ΔS
-        S_mix = range(salinity..., length = 1000)
+        S_mix = range(salinity..., length=1000)
         Θ_mix = Θₗ .+ slope .* (S_mix .- Sₗ)
         ρ_linear = total_density.(Θ_mix, S_mix, 0, fill(leos, length(S_mix)))
         ρ_l_max = maximum(ρ_linear)
@@ -69,14 +69,14 @@ end
 Δσ_linear = Δσ_upper_linear ./ Δσ_lower_linear
 
 replace!(x -> x > 10 || x < 1 ? NaN : x, Rᵨ_nleos)
-for (i, c) ∈ enumerate(eachcol(reverse(Rᵨ_nleos, dims = 1)))
+for (i, c) ∈ enumerate(eachcol(reverse(Rᵨ_nleos, dims=1)))
     for j ∈ eachindex(c)
         Δσ_nonlinear[i, j] = isnan(Rᵨ_nleos[i, j]) ? NaN : Δσ_nonlinear[i, j]
         δ_nleos[i, j] = isnan(Rᵨ_nleos[i, j]) ? NaN : δ_nleos[i, j]
     end
 end
 replace!(x -> x > 10 || x < 1 ? NaN : x, Rᵨ_leos)
-for (i, c) ∈ enumerate(eachcol(reverse(Rᵨ_leos, dims = 1)))
+for (i, c) ∈ enumerate(eachcol(reverse(Rᵨ_leos, dims=1)))
     for j ∈ eachindex(c)
         Δσ_linear[i, j] = isnan(Rᵨ_leos[i, j]) ? NaN : Δσ_linear[i, j]
         δ_leos[i, j] = isnan(Rᵨ_leos[i, j]) ? NaN : δ_leos[i, j]
@@ -99,81 +99,90 @@ find_dmol = findall(Δσ_nonlinear .> 0.95)
 markersize = 15
 linear_colour = :black
 nlinear_colour = linear_colour
-fig = Figure(size = (800, 1000))
-ax_lRᵨ = Axis(fig[1, 1], xlabel = "ΔΘ (°C)", ylabel = "ΔS (gkg⁻¹)", title = "Linear eos")
-hm = heatmap!(ax_lRᵨ, ΔΘ, ΔS, Rᵨ_leos, colormap = :amp)
-scatter!(ax_lRᵨ, arctic_obs...; color = :blue, markersize)
+fig = Figure(size=(800, 1000))
+ax_lRᵨ = Axis(fig[1, 1], xlabel="ΔΘ (°C)", ylabel="ΔS (gkg⁻¹)", title="Linear eos")
+hm = heatmap!(ax_lRᵨ, ΔΘ, ΔS, Rᵨ_leos, colormap=:amp)
+scatter!(ax_lRᵨ, arctic_obs...; color=:blue, markersize)
 scatter!(ax_lRᵨ, ΔΘ_expts, ΔS_expts_linear;
-         color = linear_colour, markersize, marker = linear_expt_markers)
-ax_nlRᵨ = Axis(fig[1, 2], xlabel = "ΔΘ (°C)", ylabel = "ΔS (gkg⁻¹)", title = "Nonlinear eos")
-hm_nlRᵨ = heatmap!(ax_nlRᵨ, ΔΘ, ΔS, Rᵨ_nleos, colormap = :amp)
-scatter!(ax_nlRᵨ, arctic_obs...; markersize, color = :blue)
+    color=linear_colour, markersize, marker=linear_expt_markers)
+ax_nlRᵨ = Axis(fig[1, 2], xlabel="ΔΘ (°C)", ylabel="ΔS (gkg⁻¹)", title="Nonlinear eos")
+hm_nlRᵨ = heatmap!(ax_nlRᵨ, ΔΘ, ΔS, Rᵨ_nleos, colormap=:amp)
+scatter!(ax_nlRᵨ, arctic_obs...; markersize, color=:blue)
 scatter!(ax_nlRᵨ, ΔΘ_expts, ΔS_expts_nlinear;
-         color = nlinear_colour, markersize, marker = nlinear_expt_markers)
-hideydecorations!(ax_nlRᵨ, grid = false, ticks = false)
-hidexdecorations!(ax_nlRᵨ, grid = false, ticks = false)
-hidexdecorations!(ax_lRᵨ, grid = false, ticks = false)
-Colorbar(fig[1, 3], hm_nlRᵨ, label = L"R_{\rho}")
+    color=nlinear_colour, markersize, marker=nlinear_expt_markers)
+hideydecorations!(ax_nlRᵨ, grid=false, ticks=false)
+hidexdecorations!(ax_nlRᵨ, grid=false, ticks=false)
+hidexdecorations!(ax_lRᵨ, grid=false, ticks=false)
+Colorbar(fig[1, 3], hm_nlRᵨ, label=L"R_{\rho}")
 
 colorrange = (minimum(Δσ_nonlinear[.!isnan.(Δσ_nonlinear)]), 1)
-ax_lR_Δρ = Axis(fig[2, 1], xlabel = "ΔΘ (°C)", ylabel = "ΔS (gkg⁻¹)")
-hm = heatmap!(ax_lR_Δρ, ΔΘ, ΔS, Δσ_linear; colorrange, colormap = :batlow)
-scatter!(ax_lR_Δρ, arctic_obs...; color = :blue, markersize)
+ax_lR_Δρ = Axis(fig[2, 1], xlabel="ΔΘ (°C)", ylabel="ΔS (gkg⁻¹)")
+hm = heatmap!(ax_lR_Δρ, ΔΘ, ΔS, Δσ_linear; colorrange, colormap=:batlow)
+scatter!(ax_lR_Δρ, arctic_obs...; color=:blue, markersize)
 scatter!(ax_lR_Δρ, ΔΘ_expts, ΔS_expts_linear;
-         color = linear_colour, markersize, marker = linear_expt_markers)
-ax_nlR_Δρ = Axis(fig[2, 2], xlabel = "ΔΘ (°C)", ylabel = "ΔS (gkg⁻¹)")
-hideydecorations!(ax_nlR_Δρ, grid = false, ticks = false)
-hm_nlR_Δρ = heatmap!(ax_nlR_Δρ, ΔΘ, ΔS, Δσ_nonlinear; colorrange, colormap = :batlow)
-scatter!(ax_nlR_Δρ, arctic_obs...; markersize, color = :blue)
+    color=linear_colour, markersize, marker=linear_expt_markers)
+ax_nlR_Δρ = Axis(fig[2, 2], xlabel="ΔΘ (°C)", ylabel="ΔS (gkg⁻¹)")
+hideydecorations!(ax_nlR_Δρ, grid=false, ticks=false)
+hm_nlR_Δρ = heatmap!(ax_nlR_Δρ, ΔΘ, ΔS, Δσ_nonlinear; colorrange, colormap=:batlow)
+scatter!(ax_nlR_Δρ, arctic_obs...; markersize, color=:blue)
 scatter!(ax_nlR_Δρ, ΔΘ_expts, ΔS_expts_nlinear;
-         color = nlinear_colour, markersize, marker = nlinear_expt_markers)
-hidexdecorations!(ax_lR_Δρ, grid = false, ticks = false)
-hidexdecorations!(ax_nlR_Δρ, grid = false, ticks = false)
-Colorbar(fig[2, 3], hm_nlR_Δρ, label = L"\delta_{\mathrm{mol}}")
+    color=nlinear_colour, markersize, marker=nlinear_expt_markers)
+hidexdecorations!(ax_lR_Δρ, grid=false, ticks=false)
+hidexdecorations!(ax_nlR_Δρ, grid=false, ticks=false)
+Colorbar(fig[2, 3], hm_nlR_Δρ, label=L"\delta_{\mathrm{mol}}")
 
 colorrange = (0, 1)
 custom_colors = [:grey75, :tomato]
 cat_cmap = cgrad(custom_colors, 2, categorical=true)
 δ_leos_cat = ifelse.(δ_leos .== 1, 0, δ_leos)
 δ_nleos_cat = ifelse.(δ_nleos .== 1, 0, δ_nleos)
-ax_δ_linear = Axis(fig[3, 1], xlabel = "ΔΘ (°C)", ylabel = "ΔS (gkg⁻¹)")
+ax_δ_linear = Axis(fig[3, 1], xlabel="ΔΘ (°C)", ylabel="ΔS (gkg⁻¹)")
 # hm_δ_linear = heatmap!(ax_δ_linear, ΔΘ, ΔS, δ_leos; colorrange, colormap = :turbid, highclip = :red, lowclip = :orange)
-hm_δ_linear = heatmap!(ax_δ_linear, ΔΘ, ΔS, δ_leos_cat; colorrange, colormap = cat_cmap)
-scatter!(ax_δ_linear, arctic_obs...; markersize, color = :blue)
-scatter!(ax_δ_linear, ΔΘ_expts, ΔS_expts_linear; color = linear_colour,
-         label = linear_expt_labels, markersize, marker = linear_expt_markers)
-ax_δ_nlinear = Axis(fig[3, 2], xlabel = "ΔΘ (°C)", ylabel = "ΔS (gkg⁻¹)")
+hm_δ_linear = heatmap!(ax_δ_linear, ΔΘ, ΔS, δ_leos_cat; colorrange, colormap=cat_cmap)
+scatter!(ax_δ_linear, arctic_obs...; markersize, color=:blue)
+scatter!(ax_δ_linear, ΔΘ_expts, ΔS_expts_linear; color=linear_colour,
+    label=linear_expt_labels, markersize, marker=linear_expt_markers)
+ax_δ_nlinear = Axis(fig[3, 2], xlabel="ΔΘ (°C)", ylabel="ΔS (gkg⁻¹)")
 # hm_δ_nlinear = heatmap!(ax_δ_nlinear, ΔΘ, ΔS, δ_nleos; colorrange, colormap = :turbid, highclip = :red, lowclip = :orange)
-hm_δ_nlinear = heatmap!(ax_δ_nlinear, ΔΘ, ΔS, δ_nleos_cat; colorrange, colormap = cat_cmap)
-scatter!(ax_δ_nlinear, arctic_obs...; markersize, color = :blue, label = "Arctic interfaces")
-scatter!(ax_δ_nlinear, ΔΘ_expts, ΔS_expts_nlinear; color = nlinear_colour,
-         label = nlinear_expt_labels, markersize, marker = nlinear_expt_markers)
-hideydecorations!(ax_δ_nlinear, grid = false, ticks = false)
-Colorbar(fig[3, 3], hm_δ_nlinear, ticks=([0.25, 0.75], [L"\delta_{\mathrm{turb}} = 1",L"\delta_{\mathrm{turb}} > 1"]), ticklabelrotation = π/2,
-         ticksvisible = false)
+hm_δ_nlinear = heatmap!(ax_δ_nlinear, ΔΘ, ΔS, δ_nleos_cat; colorrange, colormap=cat_cmap)
+scatter!(ax_δ_nlinear, arctic_obs...; markersize, color=:blue, label="Arctic interfaces")
+scatter!(ax_δ_nlinear, ΔΘ_expts, ΔS_expts_nlinear; color=nlinear_colour,
+    label=nlinear_expt_labels, markersize, marker=nlinear_expt_markers)
+hideydecorations!(ax_δ_nlinear, grid=false, ticks=false)
+Colorbar(fig[3, 3], hm_δ_nlinear, ticks=([0.25, 0.75], [L"\delta_{\mathrm{turb}} = 1", L"\delta_{\mathrm{turb}} > 1"]), ticklabelrotation=π / 2,
+    ticksvisible=false)
 
 # panel labels
 panel_labels = ["(a)", "(b)", "(c)", "(d)", "(e)", "(f)"]
 axs = [ax_lRᵨ, ax_lR_Δρ, ax_δ_linear, ax_nlRᵨ, ax_nlR_Δρ, ax_δ_nlinear]
+ΔΘ_Baltic = -1.5
+ΔΘ_Banyoles = -1.0
 for (i, a) ∈ enumerate(axs)
+    vlines!(a, ΔΘ_Baltic, color=Makie.wong_colors()[2], linestyle=:dash)
+    vlines!(a, ΔΘ_Banyoles, color=Makie.wong_colors()[3], linestyle=:dash)
     text!(a, 1, 0,
-          text = panel_labels[i],
-          font = :bold,
-          align = (:right, :bottom),
-          offset = (-4, 2),
-          space = :relative,
-          fontsize = 24
-          )
+        text=panel_labels[i],
+        font=:bold,
+        align=(:right, :bottom),
+        offset=(-4, 2),
+        space=:relative,
+        fontsize=24
+    )
 end
 # Legend
-legend_markers = [MarkerElement(color = :black, marker = m; markersize)
+legend_markers = [MarkerElement(color=:black, marker=m; markersize)
                   for m ∈ vcat(linear_expt_markers, nlinear_expt_markers)]
 legend_expts = vcat(linear_expt_labels, nlinear_expt_labels)
-Legend(fig[4, 1], legend_markers, legend_expts, "DNS Experiments",
-       orientation = :horizontal, nbanks = 2)
-lmarker_2 = [MarkerElement(color = :blue, marker = :circle; markersize)]
-lobs = ["Timmermans et al. (2008)"]
-Legend(fig[4, 2], lmarker_2, lobs, "Observations",
-       orientation = :horizontal)
+obs_marker = [MarkerElement(color=:blue, marker=:circle; markersize),
+    LineElement(color=Makie.wong_colors()[2], linestyle=:dash),
+    LineElement(color=Makie.wong_colors()[3], linestyle=:dash)]
+lobs = ["Timmermans et al. (2008)",
+    "ΔΘ max\nUmlauf et al. (2018)",
+    "ΔΘ max\nSanchez & Roget (2007)"]
+# Legend(fig[4, 1], legend_markers, legend_expts, "DNS Experiments",
+# orientation=:horizontal, nbanks=2)
+# Legend(fig[4, 2], obs_marker, lobs, "Observations",
+# orientation=:vertical)
+Legend(fig[4, :], [legend_markers, obs_marker], [legend_expts, lobs], ["DNS Experiments", "Observations"], orientation=:horizontal, nbanks=2)
 ##
 save("fig4.png", fig)
--- figures/fig7.jl
+++ figures/fig7.jl
@@ -1,38 +1,38 @@
 include("plotting_env_and_load.jl")
 ## Figure seven
-fig = Figure(size = (800, 1000))
+fig = Figure(size=(800, 1000))
 # density ratio and salinity midpoint
 ax_interface = Axis(fig[1, 1],
-                    title = "(a) Salinity interface height",
-                    xlabel = "time (min)",
-                    ylabel = L"$z*$ (m)")
+    title="(a) Salinity interface height",
+    xlabel="time (min)",
+    ylabel=L"$z*$ (m)")
 ax_R_rho = Axis(fig[2, 1],
-                title = "(b) Density ratio",
-                xlabel = "time ( min)",
-                ylabel = L"R_{\rho}")
+    title="(b) Density ratio",
+    xlabel="time ( min)",
+    ylabel=L"R_{\rho}")
 
+ts_length = 240
 for (i, file) ∈ enumerate(files)
     jldopen(file) do f
         t = f["dims"]["time"][1:ts_length]
         z✶ = f["dims"]["z✶"][f["S_interface_idx"]][1:ts_length]
         Rᵨ = f["R_ρ"][1:ts_length]
         ls = i % 2 == 0 ? :dash : :solid
-        lines!(ax_interface, t ./ 60, z✶, label = all_labels[i], linestyle = ls)
-        lines!(ax_R_rho, t ./ 60, Rᵨ, label = all_labels[i], linestyle = ls)
+        lines!(ax_interface, t ./ 60, z✶, label=all_labels[i], linestyle=ls)
+        lines!(ax_R_rho, t ./ 60, Rᵨ, label=all_labels[i], linestyle=ls)
     end
 end
-hidexdecorations!(ax_R_rho, grid = false, ticks = false)
-hidexdecorations!(ax_interface, grid = false, ticks = false)
+hidexdecorations!(ax_R_rho, grid=false, ticks=false)
+hidexdecorations!(ax_interface, grid=false, ticks=false)
 linkxaxes!(ax_R_rho, ax_interface)
 
 # energetics
 files = (l_R_ρ_105_dT2_diagnostics, nl_R_ρ_105_dT2_diagnostics,
-         l_R_ρ_105_dT1_diagnostics, nl_R_ρ_105_dT1_diagnostics,
-         l_R_ρ_105_dT05_diagnostics, nl_R_ρ_105_dT05_diagnostics)
-ax = [Axis(fig[i, 1], xlabel = "time (min)", ylabel = L"\mathcal{APE}") for i ∈ 3:4]
+    l_R_ρ_105_dT1_diagnostics, nl_R_ρ_105_dT1_diagnostics,
+    l_R_ρ_105_dT05_diagnostics, nl_R_ρ_105_dT05_diagnostics)
+ax = [Axis(fig[i, 1], xlabel="time (min)", ylabel=L"\mathcal{APE}") for i ∈ 3:4]
 ax[1].title = "(c) Available potential energy"
-ax[2].title = "(d) Time derivateive of APE, experiments I and IV"
-ts_length = 240
+ax[2].title = "(d) Time derivative of APE, experiments I and IV"
 j = 1
 linestyles = (:solid, :dash)
 for (i, f) ∈ enumerate(files)
@@ -49,18 +49,18 @@ for (i, f) ∈ enumerate(files)
         Δt = diff(t)
         dₜape = diff(ape) ./ Δt
         ls = i % 2 == 0 ? linestyles[2] : linestyles[1]
-        lines!(ax[1], 1:ts_length, ape, linestyle = ls, label = all_labels[i])
+        lines!(ax[1], 1:ts_length, ape, linestyle=ls, label=all_labels[i])
         if i < 3
-            lines!(ax[2], 2:ts_length, dₜape, linestyle = ls)
+            lines!(ax[2], 2:ts_length, dₜape, linestyle=ls)
             ax[2].ylabel = L"d$\mathcal{APE}$/dt (s)"
         end
 
     end
 end
-hidexdecorations!(ax[1], grid = false, ticks = false)
+hidexdecorations!(ax[1], grid=false, ticks=false)
 linkxaxes!(ax[1], ax[2])
 
-Legend(fig[5, 1], ax_R_rho, "Experiment", orientation = :horizontal, nbanks = 2)
+Legend(fig[5, 1], ax_R_rho, "Experiment", orientation=:horizontal, nbanks=2)
 ##
 save("fig7.png", fig)
 
